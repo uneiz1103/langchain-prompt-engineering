8a9226f4a0d84b593798e2d7b4754a3f514bb29b
uneiz1103 <uneizshaikh1103@gmail.com>
2025-07-21 13:27:34 +0530

    refactor: use LangChain message classes for clearer chat history tracking

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,6 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
 load_dotenv()
 
@@ -11,15 +12,18 @@ llm = HuggingFaceEndpoint(
 
 model = ChatHuggingFace(llm = llm)
 
-chat_history = []
+chat_history = [
+    SystemMessage(content='you are a helpful AI assistant'),
+    HumanMessage(content='Capital of USA')
+]
 
 while True:
     user_input = input('You: ')
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content = user_input))
     if user_input.strip().lower() == 'exit':
         break
     result = model.invoke(chat_history)
-    chat_history.append(result.content)
+    chat_history.append(AIMessage(content = result.content))
     print('AI: ',result.content)
 
 print(chat_history)
