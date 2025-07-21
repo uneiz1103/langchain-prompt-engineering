24bf9d5a77ea66c7fd66a8c8f24191f6c9f04fa1
uneiz1103 <uneizshaikh1103@gmail.com>
2025-07-21 17:00:53 +0530

    Implemented ChatPromptTemplate with variable placeholders

---
+++ chat_prompt_template.py
@@ -0,0 +1,15 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+chat_template = ChatPromptTemplate(
+    [   
+        ('system', 'You are a helpful {domain} expert'),
+        ('human', 'Explain in simple terms what is {topic}')
+    ]
+)
+
+prompt = chat_template.invoke({
+    'domain': 'Machine Learning',
+    'topic': ' reinforcement learning'
+})
+
+print(prompt)
